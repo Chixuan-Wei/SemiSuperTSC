6fd9c269e96efd7200613a6bb0f8dbaac63a3c88
Chixuan Wei <46340977+Chixuan-Wei@users.noreply.github.com>
2021-12-20 16:25:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ from network_run_scp import *
 
 if __name__ == "__main__":
     for alpha in [0.1, 0.3, 0.5]:
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'  # 设置设备 优先在GPU上运行
         one_run(run_time="t{}".format(str("t1")), alpha=alpha)
